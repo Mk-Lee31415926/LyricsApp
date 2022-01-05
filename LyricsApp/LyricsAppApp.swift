79756a82cc84517b80fd894832ee5baa3456daaa
Starmy <mf99250@gmail.com>
2022-01-05 15:20:31 +0800

    Initial Commit

---
+++ LyricsApp/LyricsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricsAppApp.swift
+//  LyricsApp
+//
+//  Created by User19 on 2022/1/5.
+//
+
+import SwiftUI
+
+@main
+struct LyricsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
